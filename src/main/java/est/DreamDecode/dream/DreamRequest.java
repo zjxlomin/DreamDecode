cf8567f9c9120b36504d382af38cebc84c2ab483
Tayo Bus <wnsqja6694@gmail.com>
2025-10-29 13:13:03 +0900

    Add: "Dream DTO 추가"

---
+++ src/main/java/est/DreamDecode/dream/DreamRequest.java
@@ -0,0 +1,28 @@
+package est.DreamDecode.dream;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class DreamRequest {
+  private Long id;
+  private Long userId;
+  private String title;
+  private String content;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
+  private boolean isPublic;
+
+  public Dream toEntity() {
+    Dream dream = Dream.builder()
+                          .userId(this.getUserId())
+                          .title(this.getTitle())
+                          .content(this.getContent())
+                          .isPublic(this.isPublic())
+                          .build();
+    return dream;
+  }
+}
